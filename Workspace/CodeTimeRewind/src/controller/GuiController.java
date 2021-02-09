5743c9a43fa8d4d34a0c6222a1539f05b783cd61
RABOT Mathieu <Mathieu.RABOT@cpnv.ch>
2021-02-09 14:13:00 +0100

    Ajout des librairies Slack2D et création MCD+MLD

---
+++ Workspace/CodeTimeRewind/src/controller/GuiController.java
@@ -0,0 +1,53 @@
+package controller;
+
+import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class GuiController implements Controller {
+
+	private Gui currentGui;
+	@Override
+	public void render(GameContainer gc, Graphics g, Vector2f origin) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta, Vector2f origin) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(int key, char c, GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(int key, char c, GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(int button, int x, int y) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(int oldx, int oldy, int newx, int newy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(int button, int x, int y) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Workspace/CodeTimeRewind/src/main/Game.java
@@ -0,0 +1,54 @@
+package main;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.Graphics;
+
+
+public class Game extends BasicGame {
+    
+    public Game() {
+        // Title windows name
+        super("Code::TimeRewind");
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+    	//this.currentGui = new MenuGui(null);
+    }
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer gc, int delta) throws SlickException {
+        
+    }
+    
+    @Override
+    public void keyReleased(int key, char c) {
+        
+    }
+    
+    @Override
+    public void keyPressed(int key, char c) {
+        
+    }
+    
+    @Override 
+    public void mousePressed(int button, int x, int y) {
+        
+    }
+    
+    @Override
+    public void mouseReleased(int button, int x, int y) {
+        
+    }
+    
+    @Override 
+    public void mouseMoved(int oldx, int oldy, int newx, int newy) {
+        
+    }   
+}
